7796d96f13df22b5b330e7a9d78b251271408524
techzbq <zbq_930713@163.com>
2017-02-28 15:36:10 +0800

    fix start instructions

--- bin/start-master.py
+++ bin/start-master.py
@@ -22,34 +22,41 @@ if options.loglevel:
 if options.local and not options.condor:
     # check runtime env
     try:
-        rc = subprocess.Popen(["mpich2version"])
+        rc = subprocess.Popen(["mpich2version"], stdout=subprocess.PIPE)
+        print('SETUP: find mpich tool')
     except:
         print("can't find mpich tool, please setup mpich2 first")
         exit()
 
     if 'Boost' not in os.environ['PATH']:
         print("can't find Boost.Python, this may cause some problem")
+    else:
+        print('SETUP: find Boost')
 
     # check script file
     if not options.script_file:
         print("no app script file, stop running")
         exit()
 
-    # start mpd
     if options.worker_n <= 0:
         print("worker number no less than 1")
         exit()
 
-    subprocess.Popen(["mpd&"])
+    # start mpd
+    try:
+        subprocess.Popen(["mpd&"])
+    except:
+        print("Start mpd deamon process error, exit...")
+        exit()
     print("start mpd deamon process...")
 
     print("starting master...")
     # start master
     script_file = options.script_file
-    subprocess.Popen(["mpiexec","python","RunMaster.py", script_file])
+    subprocess.Popen(["mpiexec","python","RunMaster.py", script_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # start worker
     print("starting worker...")
-    subprocess.Popen(["mpiexec", "-n",str(options.worker_n),"python", "WorkerAgent.py"])
+    subprocess.Popen(["mpiexec", "-n",str(options.worker_n),"python", "WorkerAgent.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 elif options.condor and not options.local:
     pass
